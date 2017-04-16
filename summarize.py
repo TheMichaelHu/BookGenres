75795385c3a6ad30b1cf9a3c11f5952e0fd74351
Michael <michaelanhu@gmail.com>
2017-04-16 19:14:18 -0400

    use sparse matrices, save reshaped matrix

--- summarize.py
+++ summarize.py
@@ -3,26 +3,45 @@ import pandas as pd
 from sklearn.decomposition import TruncatedSVD
 import seaborn as sns
 import matplotlib.pyplot as plt
+from scipy import sparse
 
-data = pd.read_csv('./ratings_Books.csv')
+data = pd.read_csv('./ratings_Books_small.csv')
 data = data.drop(['timestamp'], 1)
+count = 0
 
 
 def main():
     print "users", len(data['user'].unique())
     print "books", len(data['book'].unique())
+    name = "small"
 
-    df = reshape_data()
+    sparse_matrix = get_reshaped_data(name + "_reshaped_data")
+    # sparse_matrix = load_sparse_csr(name + "_reshaped_data.npz")
 
-    X = df.drop(['book'], 1).as_matrix()
     svd = TruncatedSVD(n_components=5)
-    svd.fit(X)
+    svd.fit(sparse_matrix)
 
     print svd.explained_variance_ratio_
     print svd.explained_variance_ratio_.sum()
+
     # X_hat = pd.DataFrame(pca.fit_transform(X))
     # sns.jointplot(x=0, y=1, data=X_hat, xlim=(-.68, -.66), ylim=(-.25, -.2))
     # sns.plt.show()
+
+def get_reshaped_data(name):
+    sparse_matrix = sparse.vstack(reshape_data().values)
+    save_sparse_csr(name, sparse_matrix)
+    return sparse_matrix
+
+
+def save_sparse_csr(name, matrix):
+    np.savez(name, data=matrix.data, indices=matrix.indices,
+             indptr=matrix.indptr, shape=matrix.shape)
+
+
+def load_sparse_csr(name):
+    loader = np.load(name)
+    return sparse.csr_matrix((loader['data'], loader['indices'], loader['indptr']), shape=loader['shape'])
 
 
 def reshape_data():
@@ -31,16 +50,19 @@ def reshape_data():
 
 
 def get_ratings(book):
+    global count
+    if count % 1000 == 0:
+        print count
+    count += 1
     users = pd.Series(data['user'].unique())
     ratings = data.loc[data['book'] == book].reset_index()
 
-    zeros = pd.DataFrame(np.zeros((1, len(users))), columns=users)
-    zeros['book'] = [book]
+    zeros = pd.DataFrame(np.zeros((len(users), 1)), index=users)
 
     for i in range(len(ratings['user'])):
-        zeros[ratings['user'][i]] = normalize_rating(ratings['rating'][i], book, ratings['user'][i])
+        zeros.loc[ratings['user'][i]][0] = normalize_rating(ratings['rating'][i], book, ratings['user'][i])
 
-    return zeros.loc[0]
+    return sparse.csr_matrix(zeros.values.T)
 
 
 def normalize_rating(rating, book, user):
