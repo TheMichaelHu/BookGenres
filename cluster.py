f7cd0a56c38d7b8c82b7a6a8908c7287d9d0e6d9
Michael <michaelanhu@gmail.com>
2017-04-19 01:20:32 -0400

    keep copy of books around

--- cluster.py
+++ cluster.py
@@ -18,15 +18,19 @@ else:
 
 
 def main():
+    data = pd.read_csv('./ratings_Books_' + name + '.csv')
+    books = pd.Series(data['book'].unique(), name="book")
     sparse_matrix = load_sparse_csr(name + "_reshaped_data.npz")
 
     svd = TruncatedSVD(n_components=7)
     svd.fit(sparse_matrix)
-    data = svd.fit_transform(sparse_matrix)
+    data = pd.DataFrame(svd.fit_transform(sparse_matrix))
 
     # keep dropping lowest of 3 clusters
-    for i in range(10):
-        hac = AgglomerativeClustering(n_clusters=3, affinity='cosine', linkage='average')
+    num_clusters = 3
+    clusters_dropped = 0
+    while clusters_dropped < 50:
+        hac = AgglomerativeClustering(n_clusters=num_clusters, affinity='cosine', linkage='average')
         hac.fit(data)
         clusters = hac.fit_predict(data)
         cluster_counts = {}
@@ -37,37 +41,44 @@ def main():
                 cluster_counts[cluster] = 1
         count_order = np.argsort(cluster_counts.values())[::-1]
 
-        print "iter ", i, ": dropped ", cluster_counts[count_order[-1]]
+        if cluster_counts[count_order[-1]] <= 100:
+            num_clusters = 3
+            clusters_dropped += 1
+            print "iter ", clusters_dropped, ": dropped ", cluster_counts[count_order[-1]]
+            data, books = keep_clusters(clusters, count_order[:2], data, books)
+        else:
+            num_clusters += 1
 
-        data = keep_clusters(clusters, count_order[:2], data)
+    for num_clusters in range(2, 5):
+        print "_" * 100
+        hac = AgglomerativeClustering(n_clusters=num_clusters, affinity='cosine', linkage='average')
+        hac.fit(data)
+        clusters = hac.fit_predict(data)
 
-    num_clusters = 2
-    hac = AgglomerativeClustering(n_clusters=num_clusters, affinity='cosine', linkage='average')
-    hac.fit(data)
-    clusters = hac.fit_predict(data)
-
-    # print_clusters(clusters, num_clusters, data)
-
-    # plot_variance(data, 21)
+        print_clusters(clusters, num_clusters, data, books)
 
     labels = hac.labels_
-    labels = get_custom_labels(clusters)
+    # labels = get_custom_labels(clusters, books)
 
     plot_dendrogram(hac, labels=labels)
     plt.show()
 
+    # plot_variance(data, 21)
 
-def get_custom_labels(clusters):
-    data = pd.read_csv('./ratings_Books_' + name + '.csv')
-    books = pd.Series(data['book'].unique(), name="book")
-    labels = [str(x) for x in clusters]
+    plot_dendrogram(hac, labels=labels, truncate_mode='lastp', p=4)
+    plt.show()
+
+
+def get_custom_labels(clusters, books):
+    labels = pd.Series([str(x) for x in clusters], index=books.index)
     mappings = {"0439064864": "Harry Potter 2", "0439136350": "Harry Potter 3", "0590997297": "Animorphs 9", "0590997289": "Animorphs 8"}
 
     for isbn in mappings:
-        idx = books[books == isbn].index[0]
-        labels[idx] = mappings[isbn] + " " + labels[idx]
+        if len(books[books == isbn]) != 0:
+            idx = books[books == isbn].index[0]
+            labels[idx] = mappings[isbn] + " " + labels[idx]
 
-    return labels
+    return labels.values
 
 
 def plot_2d_data(matrix):
@@ -95,7 +106,7 @@ def plot_variance(data, limit):
 def get_within_cluster_variation(clusters, n_clusters, data):
     total_var = 0
     for cluster_num in range(n_clusters):
-        df = pd.concat([pd.DataFrame(data), pd.Series(clusters, name="cluster")], axis=1)
+        df = pd.concat([data, pd.Series(clusters, name="cluster", index=data.index)], axis=1)
         df = df[df["cluster"] == cluster_num]
         dists = pdist(df.drop(['cluster'], 1).values, metric="cosine")
         variance = dists.sum()/len(dists)
@@ -108,16 +119,14 @@ def get_within_cluster_variation(clusters, n_clusters, data):
     return total_var
 
 
-def print_clusters(clusters, n_clusters, data):
+def print_clusters(clusters, n_clusters, data, books):
     for cluster_num in range(n_clusters):
         print "Cluster", cluster_num, ":"
-        data = pd.read_csv('./ratings_Books_' + name + '.csv')
-        books = pd.Series(data['book'].unique(), name="book")
-        df = pd.concat([books, pd.Series(clusters, name="cluster")], axis=1)
+        df = pd.concat([books, pd.Series(clusters, name="cluster", index=books.index)], axis=1)
         df = df.loc[df["cluster"] == cluster_num]
         cluster_books = df["book"].values
 
-        sample_isbns = np.random.choice(cluster_books, 5)
+        sample_isbns = np.random.choice(cluster_books, 20)
         print [(x + ": " + get_title(x)) for x in sample_isbns]
 
 
@@ -132,16 +141,22 @@ def get_title(isbn):
     return json.loads(res[18:-1])['ISBN:' + isbn]['title']
 
 
-def keep_clusters(clusters, cluster_nums, data):
-    df = pd.concat([pd.DataFrame(data), pd.Series(clusters, name="cluster")], axis=1)
+def keep_clusters(clusters, cluster_nums, data, books):
+    # whitelist = ["0439064864", "0439136350", "0590997297", "0590997289"]
+    df = pd.concat([data, pd.Series(clusters, name="cluster", index=data.index), books], axis=1)
+    # df_whitelist = df[df["book"].isin(whitelist)]
     df = df[df["cluster"].isin(cluster_nums)]
-    return df.drop(['cluster'], 1).values
+    # df = df_whitelist.combine_first(df)
+    return df.drop(['cluster', 'book'], 1), df['book']
 
 
-def remove_clusters(clusters, cluster_nums, data):
-    df = pd.concat([pd.DataFrame(data), pd.Series(clusters, name="cluster")], axis=1)
+def remove_clusters(clusters, cluster_nums, data, books):
+    # whitelist = ["0439064864", "0439136350", "0590997297", "0590997289"]
+    df = pd.concat([data, pd.Series(clusters, name="cluster", index=data.index), books], axis=1)
+    # df_whitelist = df[df["book"].isin(whitelist)]
     df = df[~df["cluster"].isin(cluster_nums)]
-    return df.drop(['cluster'], 1).values
+    # df = df_whitelist.combine_first(df)
+    return df.drop(['cluster', 'book'], 1), df['book']
 
 
 def plot_dendrogram(hac, **kwargs):
